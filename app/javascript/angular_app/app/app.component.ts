48fd14d7a57060c9c2f98782b80ff4bc53e4e219
Ryan Grow <ryan.grow@bluemoondigital.co>
2017-09-23 18:59:49 -0600

    feat(ngx-bootstrap): Added and configured ngx-bootstrap package.
    Include a timepicker on the main page to show how this can be included. We can evaluate this and
    determine whether or not ngx-bootstrap is the way to go, but I got it working.

--- app/javascript/angular_app/app/app.component.ts
+++ app/javascript/angular_app/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'angular-app',
-  template: `<h1>Project: {{name}}</h1>`
+  template: `
+    <h1>Project: {{name}}</h1>
+    <timepicker [(ngModel)]="time"></timepicker>
+    <pre>Selected time: {{time | json}}</pre>
+  `
 })
-export class AppComponent implements OnInit {
-  
-  name: string;
 
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  name: string;
+  time: Date = new Date();
+  constructor() {
+  }
 
   ngOnInit() {
-
     this.name = 'Viacom Tagging Project';
-
   };
 }
--- app/javascript/angular_app/app/app.module.ts
+++ app/javascript/angular_app/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    TimepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
