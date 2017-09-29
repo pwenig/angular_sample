78ec390ed41ed2854735e7664ab4c3db5c15e4a1
Paul Wenig <pwenig@gmail.com>
2017-09-29 14:08:57 -0600

    Adding validation tests

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Program, type: :model do
+
+  let :network {Network.create(name: 'Comedy Central', abbrev: 'CCL')}
+  subject {Program.new(abbrev: "Foo", network: network) }
+
   it 'should belong to network' do
     # Using shoulda matchers for associations
     should belong_to(:network)
@@ -12,4 +16,17 @@ RSpec.describe Program, type: :model do
     expect(program.name).to include('Clusterfest')
     expect(program.network.name).to include('Comedy Central')
   end
+
+  it 'should validate :abbrev required' do 
+    should validate_presence_of :abbrev
+  end 
+
+  it 'should validate :name required' do 
+    should validate_presence_of :name
+  end 
+
+  it 'should validate :name uniqueness' do 
+    should validate_uniqueness_of :name
+  end
+
 end
