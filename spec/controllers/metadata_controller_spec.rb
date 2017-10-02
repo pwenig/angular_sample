81a5d6c57bb869d6c5b7cdbda81af089594f1264
Ryan Grow <ryan.grow@bluemoondigital.co>
2017-10-02 16:58:42 -0600

    use devise controller helper to login before requesting

--- spec/controllers/metadata_controller_spec.rb
+++ spec/controllers/metadata_controller_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe MetadataController do 
+RSpec.describe MetadataController do
 
-  describe 'GET /metadata' do 
+  describe 'GET /metadata' do
+
+    let!(:user) {
+      User.create!(email: 'test@example.com', password: 'testing')
+    }
 
     before(:each) do
+      sign_in(user)
       network = Network.create(name: 'Comedy Central', abbrev: 'CCL')
       Program.create(name: 'Clusterfest', abbrev: 'CLTF', network_id: network.id)
-    end 
+    end
 
-    it 'fetches metadata' do 
+    it 'fetches metadata' do
       get :index
       metadata = JSON.parse(response.body)
       expect(metadata['networks'].length).to_not eq(0)
       expect(metadata['networks'].length).to eq(1)
     end
 
-  end 
-  
-end 
+  end
+
+end
