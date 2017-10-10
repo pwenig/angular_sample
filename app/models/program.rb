8ec37648914e16583496dcae18284f5ee2da33ef
Paul Wenig <pwenig@gmail.com>
2017-10-09 20:01:41 -0600

    Program constraint changes

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,8 +2,8 @@ class Program < ApplicationRecord
   belongs_to :network
   has_many :campaign_inputs
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, scope: :network_id
   validates_presence_of :abbrev
-  validates_uniqueness_of :abbrev
+  validates_uniqueness_of :abbrev, scope: :network_id
   validates_presence_of :network
 end
--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Program, type: :model do
-  let(:network) { Network.create(name: 'Comedy Central', abbrev: 'CCL') }
+  let(:network) { Network.create!(name: 'Comedy Central', abbrev: 'CCL') }
   subject { Program.new(abbrev: 'Foo', network: network) }
 
   it 'should belong to network' do
@@ -14,10 +14,26 @@ RSpec.describe Program, type: :model do
   end
 
   it 'creates a program' do
-    network = Network.create(name: 'Comedy Central', abbrev: 'CCL')
-    program = Program.create(name: 'Clusterfest', abbrev: 'CLTF', network_id: network.id)
+    network = Network.create!(name: 'Comedy Central', abbrev: 'CCL')
+    program = Program.create!(name: 'Clusterfest', abbrev: 'CLTF', network_id: network.id)
     expect(program.name).to include('Clusterfest')
     expect(program.network.name).to include('Comedy Central')
+  end
+
+  it 'creates a program with the same and on different networks' do
+    network1 = Network.create!(name: 'Comedy Central', abbrev: 'CCL')
+    network2 = Network.create!(name: 'Comedy Central Two', abbrev: 'CCL2')
+    Program.create!(name: 'Clusterfest', abbrev: 'CLTF', network_id: network1.id)
+    program2 = Program.create!(name: 'Clusterfest', abbrev: 'CLTF', network_id: network2.id)
+    expect(program2.name).to include('Clusterfest')
+  end
+
+  it 'does not create a program with the same and on the same network' do
+    network1 = Network.create!(name: 'Comedy Central', abbrev: 'CCL')
+    Program.create!(name: 'Clusterfest', abbrev: 'CLTF', network_id: network1.id)
+    program2 = Program.new(name: 'Clusterfest', abbrev: 'CLTF', network_id: network1.id)
+    expect(program2).to_not be_valid
+    expect(program2.errors[:name]).to include('has already been taken')
   end
 
   it 'should validate :abbrev required' do
@@ -29,6 +45,6 @@ RSpec.describe Program, type: :model do
   end
 
   it 'should validate :name uniqueness' do
-    should validate_uniqueness_of :name
+    should validate_uniqueness_of(:name).scoped_to(:network_id)
   end
 end
