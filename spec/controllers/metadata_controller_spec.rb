f7fd2d90bda4c261a874ffa2721d4f5304cb8bd5
Paul Wenig <pwenig@gmail.com>
2017-10-12 11:16:47 -0600

    Package Input service WIP

--- spec/controllers/metadata_controller_spec.rb
+++ spec/controllers/metadata_controller_spec.rb
@@ -10,13 +10,26 @@ RSpec.describe MetadataController do
       sign_in(user)
       network = Network.create(name: 'Comedy Central', abbrev: 'CCL')
       Program.create(name: 'Clusterfest', abbrev: 'CLTF', network_id: network.id)
+      Season.create!(name: 'S00', abbrev: 's00')
+      CampaignType.create!(name: 'Binge', abbrev: 'BG')
+      Campaign.create!(name: 'Foo', abbrev: 'F01')
+      Agency.create(name: 'Sterling Cooper', abbrev: 'sc')
+      BuyMethod.create!(name: 'CPA', abbrev: 'CPA')
+      Publisher.create!(name: 'ABC', abbrev: 'ABCX')
     end
 
     it 'fetches metadata' do
       get :index
       metadata = JSON.parse(response.body)
-      expect(metadata['networks'].length).to_not eq(0)
       expect(metadata['networks'].length).to eq(1)
+      expect(metadata['networks'].first['programs'].length).to eq(1)
+      expect(metadata['seasons'].length).to eq(1)
+      expect(metadata['campaign_types'].length).to eq(1)
+      expect(metadata['campaigns'].length).to eq(1)
+      expect(metadata['agencies'].length).to eq(1)
+      expect(metadata['buy_methods'].length).to eq(1)
+      expect(metadata['publishers'].length).to eq(1)
+      
     end
   end
 end
