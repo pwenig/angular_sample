a77d53c4f0610e3a9efae5fc7cef43024049f796
Paul Wenig <pwenig@gmail.com>
2017-11-01 20:15:53 -0600

    Creative Group model, admin, tests, rake

---
+++ spec/models/creative_group_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe CreativeGroup, type: :model do
+  subject { CreativeGroup.new(abbrev: 'AON') }
+
+  it 'should create a creative group' do
+    creative_group = CreativeGroup.create!(name: 'Always On', abbrev: 'AON')
+    expect(creative_group.name).to include('Always On')
+    expect(creative_group.abbrev).to include('AON')
+  end
+
+  it 'should validate :abbrev required' do
+    should validate_presence_of :abbrev
+  end
+
+  it 'should validate :name required' do
+    should validate_presence_of :name
+  end
+
+  it 'should validate :name uniqueness' do
+    should validate_presence_of :name
+  end
+end
