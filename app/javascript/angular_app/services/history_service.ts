0631545fbff1a17ccae129eecb49cbc01fdb1593
Paul Wenig <pwenig@gmail.com>
2017-11-13 20:03:25 -0700

    Exporting session inputs

---
+++ app/javascript/angular_app/services/history_service.ts
@@ -0,0 +1,24 @@
+import {Component, Injectable, Inject} from '@angular/core';
+
+@Injectable()
+export class HistoryService {
+
+  constructor() {}
+
+  // Stores the input so it can be exported
+  storeInput(inputObj) {
+    var stored = [];
+    stored = JSON.parse(localStorage.getItem('inputs'));
+    if(stored && stored.length > 0) {
+      stored.push(inputObj);
+      localStorage.setItem('inputs', JSON.stringify(stored))
+    }
+    else {
+      var stored = [];
+      stored.push(inputObj);
+      localStorage.setItem('inputs', JSON.stringify(stored))
+    
+    }
+  }
+
+}
