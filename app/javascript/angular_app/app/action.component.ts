f4a2c3ab39043c46e320f858268fc63b0dbca41e
Paul Wenig <pwenig@gmail.com>
2018-01-23 13:13:05 -0700

    Disabled actions that have not been developed yet

--- app/javascript/angular_app/app/action.component.ts
+++ app/javascript/angular_app/app/action.component.ts
@@ -5,10 +5,10 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   template: `
   <div class="btn-group actions">
     <button class="btn btn-primary action" (click)="actionSelected('Campaign')" [disabled]="disableNewCampaign">New Campaign</button>
-    <button class="btn btn-primary action" (click)="actionSelected('Copy/Create')" [disabled]="namestringSelected">Copy/Create</button>
+    <button class="btn btn-primary action" (click)="actionSelected('Copy/Create')" [disabled]="true">Copy/Create</button>
     <button class="btn btn-primary action" (click)="actionSelected('Edit')" [disabled]="namestringSelected">Edit</button>
-    <button class="btn btn-primary action" (click)="actionSelected('Delete')" [disabled]="namestringSelected">Delete</button>
-    <button class="btn btn-primary action" (click)="actionSelected('Export')" [disabled]="namestringSelected">Export</button>
+    <button class="btn btn-primary action" (click)="actionSelected('Delete')" [disabled]="true">Delete</button>
+    <button class="btn btn-primary action" (click)="actionSelected('Export')" [disabled]="true">Export</button>
     <button class="btn btn-primary action" *ngIf="selectedNameString.child" (click)="actionSelected(selectedNameString.child)" [disabled]="namestringSelected">New {{selectedNameString.child}}</button>
   </div>
 
@@ -23,6 +23,9 @@ export class ActionComponent implements OnInit {
   @Output() namestringAction = new EventEmitter();
 
 
+  // Change disabled selectors form [disabled]="true" back to:
+  // [disabled]="namestringSelected"
+  
   ngOnInit() {
     this.selectedNameString.child = 'Package';
   }
